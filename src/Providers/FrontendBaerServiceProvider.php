813ad2e1f5b87e20fb4690da2a9dee7ebfdad339
Ihab Hussein <ihab.hussein@plentymarkets.com>
2018-02-12 18:23:27 +0100

    - initial data

---
+++ src/Providers/FrontendBaerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EbayFrontendBaer\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class FrontendBaerServiceProvider
+ * @package EbayFrontendBaer\Providers
+ */
+class FrontendBaerServiceProvider extends ServiceProvider {
+
+    public function register() {
+
+    }
+
+}
